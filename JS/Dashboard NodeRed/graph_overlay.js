c933abadb86604f56fd1004a469b2f62337cae39
Francesco-PwC <Francesco-PwC@github.com>
2020-03-31 18:56:41 +0200

    Update graph_overlay.js

--- JS/Dashboard NodeRed/graph_overlay.js
+++ JS/Dashboard NodeRed/graph_overlay.js
@@ -154,4 +154,4 @@ function deleteOptionalOnHide(element_list){
 var ref_cont = document.getElementById("containerHeart");
 var ref_canva = document.getElementById("heartChart");
 var ref_over = ref_cont.getElementsByClassName("graph_overlay")[0];
-getPointOnclick(ref_canva,heartChart,ref_over);
+getPointOnclick(ref_canva,myChart_HEART,ref_over);
